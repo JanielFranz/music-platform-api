4ced1635d9111aaaafedf1f4a1354b04f3cdc4ae
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-07-25 14:22:47 -0500

    feat: returning file info in storage controller

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,9 +1,11 @@
 const {storageModel} = require('../models')
 
 const uploadFile = async (req, res) =>{
-    const {body} = req
+    const {body, file} = req
     const data = await storageModel.create(body)
-    res.send(data)
+    console.log(file)
+    res.send({file})
 }
-
+//If we use {} in the import we will use {} too
+//file show us the information of the file that we uploaded, we can use this info later to use filters or validations
 module.exports = {uploadFile}
