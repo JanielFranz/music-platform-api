1edc5da9b77ef8067266c87887e30147aa51e9e6
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-07-26 14:42:18 -0500

    fix: create from storage controller with try and catch

--- controllers/storage.js
+++ controllers/storage.js
@@ -52,18 +52,22 @@ const deleteFile = async(req,res) =>{
 }
 
 
-const uploadFile = async (req, res) =>{
-    const {body, file} = req
-    //
-    //we use fileData to show the information in the response
-    const fileData = {
-        filename: file.filename,
-        url: `${PUBLIC_URL}/${file.filename}`
+const uploadFile = async (req, res) => {
+    try {
+        const {file} = req
+        //
+        //we use fileData to show the information in the response
+        const fileData = {
+            filename: file.filename,
+            url: `${PUBLIC_URL}/${file.filename}`
+        }
+        //making the post request to the database
+        const data = await storageModel.create(fileData)
+        console.log(fileData.filename)
+        res.send(data)
+    } catch (e) {
+        handleHttpError(res, "No se pudo crear el archivo")
     }
-    //making the post request to the database
-    const data= await storageModel.create(fileData)
-    console.log(fileData.filename)
-    res.send(data)
 }
 //If we use {} in the import we will use {} too
 //file show us the information of the file that we uploaded, we can use this info later to use filters or validations
