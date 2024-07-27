3219275f958afe0251ab77c4424370299b3d4bdc
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-07-27 00:14:19 -0500

    feat: register route persisting in the db with hashed password

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {validatorLoginUser, validatorRegisterUser} = require('../validators/auth')
 const {matchedData} = require("express-validator");
 const {encrypt, compare} = require("../utils/handlePassword")
+const {usersModel} = require("../models/index")
 //POST
 
 //I want to have two routers, login and register
@@ -11,7 +12,8 @@ router.post("/register", validatorRegisterUser,async (req,res) =>{
     req = matchedData(req) //req cleaned
     const password = await encrypt(req.password) //encrypt password
     const body = {...req, password} //we get all the data from req and add the password or replace it if it exists
-    res.send({data: body})
+    const data = await usersModel.create(body) //persisting data to the database
+    res.send({data})
 })
 
 module.exports = router
