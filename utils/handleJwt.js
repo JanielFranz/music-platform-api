84025696e9245d77139debf2f4e775b15e9f38ae
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-07-27 23:55:26 -0500

    chore: comment about user.id in handleJwt

--- utils/handleJwt.js
+++ utils/handleJwt.js
@@ -6,6 +6,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
  * @returns {Promise<*>}
  */
 const tokenSign = async (user) =>{
+    console.log("el id es", user._id) //here it could be user.id or user._id, it works but the presentation of the value is different
     const sign = jwt.sign(
         { //set the payload
             _id : user.id,
